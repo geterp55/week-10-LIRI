dae609703acee02bf07f89c92ac710ef23b72683
Peter Guerrero <geterp@hotmail.com>
2016-07-25 10:04:25 -0500

    noks

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'a8G0YoNc6vWlfSPXxKAaMMjZ7',
-  consumer_secret: 'OBzB1zQWD6H41nrhrBZ1kCZbtkLljTsY6lDp6Sc06ZdAz34gFS',
-  access_token_key: '746368231154163714-sCGCqLrepvyfVJBNxM360bFF8p0HyVR',
-  access_token_secret: 'q9fj1DW9CYSIvIH3zgpCpf6wRFtfXxQ0RcfqZKYVH5g6F',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
 }
